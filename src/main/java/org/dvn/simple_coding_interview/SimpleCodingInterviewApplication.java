f04b92dd637fc1ef7971a5dcecfd3b33193fac78
dmitry <glorynewfound@yandex.ru>
2025-01-26 18:49:54 +0300

    initial commit

---
+++ src/main/java/org/dvn/simple_coding_interview/SimpleCodingInterviewApplication.java
@@ -0,0 +1,13 @@
+package org.dvn.simple_coding_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCodingInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCodingInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dvn/simple_coding_interview/SimpleCodingInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dvn.simple_coding_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCodingInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
